aa7540f3541a2df40098aa2c9095b99b6ce160bf
Joseph-1989 <akmal.khasanov1989@gmail.com>
2024-05-07 03:20:59 +0900

    feat: integration of environmental variables

---
+++ src/server.ts
@@ -0,0 +1,8 @@
+// Architectural pattern: MVC, Dependency Injection, MVP
+
+// Design pattern: Middleware, Decorator
+
+import dotenv from "dotenv";
+dotenv.config();
+console.log("PORT:", process.env.PORT);
+console.log("MONGO_URL:", process.env.MONGO_URL);
