07584cc4f706758fc230b0d38e355c22fbd64787
Joseph-1989 <akmal.khasanov1989@gmail.com>
2024-06-10 14:21:27 +0900

    feat: frontend related js, css, img creation

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -1,8 +1,8 @@
 import { Request, Response } from "express";
 import { T } from "../libs/types/common";
 import MemberService from "../models/Member.service";
-import { MemberType } from "../libs/enums/member.enum";
 import { LoginInput, MemberInput } from "../libs/types/member";
+import { MemberType } from "../libs/enums/member.enum";
 
 const memberService = new MemberService(); //create an instance of service class
 
@@ -10,62 +10,59 @@ const restaurantController: T = {};
 restaurantController.goHome = (req: Request, res: Response) => {
   try {
     console.log("goHome");
-    res.send("Home Page");
+    res.render("home");
   } catch (err) {
-    console.log("Error, goHome:", err);
-  }
-};
-restaurantController.getLogin = (req: Request, res: Response) => {
-  try {
-    console.log("getLogin");
-    res.send("Login Page");
-  } catch (err) {
-    console.log("Error, getLogin:", err);
+    console.log("Error: Go Home", err);
   }
 };
 
 restaurantController.getSignup = (req: Request, res: Response) => {
   try {
     console.log("getSignup");
-    res.send("Signup Page");
+    res.render("signup");
   } catch (err) {
-    console.log("Error, getSignup:", err);
+    console.error("Error : Get SignUp", err);
+  }
+};
+
+restaurantController.getLogin = (req: Request, res: Response) => {
+  try {
+    console.log("getLogin");
+    res.render("login");
+  } catch (err) {
+    console.error("Error : Get Login", err);
+  }
+};
+
+restaurantController.processSignup = async (req: Request, res: Response) => {
+  try {
+    console.log("processSignup: this is restaurant.controller module!"); //
+    console.log("the body information contains in postman app: ", req.body); //  for test
+    const newMember = req.body; // get the data from client side to server side by using 'body-parser' middleware
+    newMember.memberType = MemberType.RESTAURANT; //set the default member type to be "Restaurant"
+    const result = await memberService.processSignup(newMember);
+
+    // TODO: SESSIONS ATHENTICATION - save user info into session object and send it back to client side
+
+    res.send(result);
+  } catch (err) {
+    //    catch any error that may occur during the execution of the function
+    console.error("Error : processSignup ", err); //  print out the error message if any error occurs when processing sign up request
+    res.send(err); //return error message back to client side if any error occurs during sign up process
   }
 };
 
 restaurantController.processLogin = async (req: Request, res: Response) => {
   try {
     console.log("processLogin");
-    console.log(
-      "ProcessLogin: the body information contains in postman app: ",
-      req.body
-    );
-    const input: LoginInput = req.body;
-    // input.memberType = MemberType.RESTAURANT;
-    const memberService = new MemberService();
-    const result = await memberService.processLogin(input);
-    res.send(result);
-    res.send("processLogin Page: DONE");
+    console.log("ProcessLogin: ", req.body); //  JSON data you sent to server
+    const input: LoginInput = req.body; //   get data from client side
+    const result = await memberService.processLogin(input); //    call service class function
+    // TODO: SESSIONS ATHENTICATION
+    res.send(result); // send back the response to client side
   } catch (err) {
-    console.error("Error : processLogin ", err);
-    res.send(err);
-  }
-};
-
-restaurantController.processSignup = async (req: Request, res: Response) => {
-  try {
-    console.log("processSignup: this is restaurant.controller module!");
-    console.log("the body information contains in postman app: ", req.body);
-
-    const newMember = req.body; // get the data from client side to server side by using 'body-parser' middleware
-    newMember.memberType = MemberType.RESTAURANT; //set the default member type to be "Restaurant"
-
-    const memberService = new MemberService();
-    const result = await memberService.processSignup(newMember);
-    res.send(result);
-  } catch (err) {
-    console.error("Error : processSignup ", err);
-    res.send(err);
+    console.error("Error : processLogin ", err); //  print error message on console
+    res.send(err); // send back a response with status code and error message
   }
 };
 
--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,42 @@
-// Architectural pattern: MVC, Dependency Injection, MVP
+// console.log("EXECUTED!");
+// import moment from "moment";
+// const currentTime = moment().format("YYYY:MM:DD HH:mm");
+// console.log(currentTime);
+// const person: string = "Martin";
+// const count: number = 100;
 
-// Design pattern: Middleware, Decorator
+// Ushbu architectural va Design patternlarni yaxshi bilib olsangiz, programming
+// language dan boshqa programming language ga o`tish qiyinchilik tug`dirmaydi.
+// Architectural pattern:
+// 1. Backend: MVC (model view controller), DI (Dependency  Injection),
+// 2. Frontend: MVP (Modal view presenter)
+//  Design patterns: Middleware, Decorator
 
-// • CLUSTER => • DATABASE • => • COLLECTION • => • DOCUMENT
+// Architectural pattern, bu backend yoki frontend ni qurib beruvchi suyagi bo`ladi. Backend da ma`lumotlar oqimini
+// tartibga soluvchi vosita, arxitekturasi hisoblanadi.
+// Design pattern: bu backend da ma`lum bir bo`laklarni strukturasini yechishda xizmat qiladigan
+// vositalar. Design pattern hisoblanadi.
 
 import dotenv from "dotenv";
 dotenv.config();
-console.log("PORT:", process.env.PORT);
-console.log("MONGO_URL:", process.env.MONGO_URL);
-
-import mongoose from "mongoose";
 import app from "./app";
-
+// CLUSTER => DATABASE => COLLECTION => DOCUMENTS
+import mongoose from "mongoose";
 mongoose
   .connect(process.env.MONGO_URL as string, {})
   .then((data) => {
-    console.log("MongoDB connection succeed");
-    const PORT = process.env.PORT ?? 3003;
-    app.listen(PORT, function () {
-      console.log("The server is running successfully on port: ${PORT}");
+    //data bu mongoose.connect methodining Promise ning resolve parametrini bildiradi
+    // The `data` parameter might not be used in this block, or it might represent some internal
+    // information about the connection (though it's not typical to use `data` for this purpose).
+    console.log("MongoDB connected successfully...");
+    const PORT = process.env.PORT ?? 3003; // Attempts to retrieve a port number from the PORTenvironment variable.
+    // If PORTis undefined, defaults to port 3003.
+    app.listen(PORT, () => {
+      // Starts listening for incoming HTTP  requests on the specified host and port.
+      console.info(`Server is running successfully on port ${PORT}`);
+      console.info(`Admin project on htttp://localhost:${PORT}/admin \n`);
     });
   })
-  .catch((err) => console.log("ERROR on connection MongoDB", err));
+  .catch((err) =>
+    console.log("MongoDB: Error on connection to MONGODB: ", err)
+  );
